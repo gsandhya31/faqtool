7dd15c3c0a07480eb57c0f9ec91423c379b34f13
gsandhya31 <47244657+gsandhya31@users.noreply.github.com>
2025-09-21 11:42:08 +0530

    Update vite.config.ts
    Added for publishing n Github instead of lovable

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // <--- add this line:
+  base: '/faqtool/',
+  
   server: {
     host: "::",
     port: 8080,
